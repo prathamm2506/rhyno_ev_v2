17525aace85bb100c42ea486dfd1cecfd06c2436
Pratham Matkar <prathammatkar2506@gmail.com>
2024-05-31 11:36:34 +0530

    navbar closing

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,16 +5,7 @@ import { Link } from 'react-router-dom';
 import { Link as ScrollLink } from 'react-scroll';
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleToggle = () => {
-    setIsOpen(!isOpen);
-  };
-
-  const closeNavbar = () => {
-    setIsOpen(false);
-  };
-
+  
   const [isSearchOpen, setSearchOpen] = useState(false);
   const [isNavOpen, setNavOpen] = useState(false);
   const [isProductsOpen, setProductsOpen] = useState(false); // New state for products dropdown
@@ -39,13 +30,13 @@ const Navbar = () => {
   };
 
   return (
-    <nav className={`nav ${isOpen ? "open" : ""}`}>
-      <Link to="/" onClick={closeNavbar}>
+    <nav className={`nav ${isSearchOpen ? 'openSearch' : ''} ${isNavOpen ? 'openNav' : ''}`}>
+      <Link to="/">
         <img src={Logo} alt="" className="logo" />
       </Link>
       <ul className="nav-linksss">
         <i className="fa-solid fa-xmark navCloseBtn" onClick={closeNav} id="nav-toggler"></i>
-        <li><Link to="/" onClick={closeNavbar}>Home</Link></li>
+        <li><Link to="/">Home</Link></li>
         <li
           className="dropdown-wrapper"
           onMouseEnter={toggleProducts}
@@ -57,25 +48,25 @@ const Navbar = () => {
             <ul className="dropdown">
               <li>
                 <ScrollLink to="ourProducts" smooth={true} duration={500} onClick={closeNav}>
-                  <Link to='/SE03' onClick={closeNavbar}>SE03</Link>
+                  <Link to='/SE03'>SE03</Link>
                 </ScrollLink>
               </li>
               <li>
                 <ScrollLink to="ourProducts" smooth={true} duration={500} onClick={closeNav}>
-                <Link to='/SE03MAX' onClick={closeNavbar}>SE03 MAX</Link>
+                <Link to='/SE03MAX'>SE03 MAX</Link>
                 </ScrollLink>
               </li>
               <li>
                 <ScrollLink to="ourProducts" smooth={true} duration={500} onClick={closeNav}>
-                <Link to='/SE03LITE' onClick={closeNavbar}>SE03 LITE</Link>
+                <Link to='/SE03LITE'>SE03 LITE</Link>
                 </ScrollLink>
               </li>
             </ul>
           )}
         </li>
-        <li><a href="/Compare" onClick={closeNavbar}>Compare</a></li>
-        <li><Link to='/AboutUs' onClick={closeNavbar}>About Us</Link></li>
-        <li><Link to='/ContactUs' onClick={closeNavbar}>Contact Us</Link></li>
+        <li><a href="/Compare">Compare</a></li>
+        <li><Link to='/AboutUs'>About Us</Link></li>
+        <li><Link to='/ContactUs'>Contact Us</Link></li>
       </ul>
 
       <button className="search-icon btn btn-nav" id="searchIcon"><a href="https://wa.me/9023987528" target='_blank'> Order Now </a></button>
