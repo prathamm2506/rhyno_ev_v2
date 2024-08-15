363bac6edd94ee009ed0e127501a199209890564
Pratham Matkar <prathammatkar2506@gmail.com>
2024-08-15 11:57:53 +0530

    Navbar done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,48 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import './Navbar.css'; // Import the CSS file
+import './Navbar.css';
 import Logo from "../../assets/Logo.png";
-import gsap from 'gsap';
-import { useGSAP } from "@gsap/react";
 import { Link } from 'react-router-dom';
-import { Link as ScrollLink } from 'react-scroll';
 
 const Navbar = () => {
-
-  // const tl = gsap.timeline();
-  // useGSAP(() => {
-  //   tl.fromTo('.nav', {
-  //     delay: 1,
-  //     duration: 2,
-  //     y: "-100%",
-  //     opacity: 0,
-  //     ease: 'power2.out'
-  //   },
-  //     {
-  //       y: 0,
-  //       opacity: 1,
-  //       ease: 'power4.out'
-  //     })
-  //   .fromTo('.nav-linksss li', {
-  //     duration: 0.1,
-  //     y: "-30%",
-  //     opacity: 0,
-  //     scale: 0.5,
-  //     ease: 'power4.out',
-  //   },
-  //   {
-  //     y: 0,
-  //     opacity: 1,
-  //     scale: 1,
-  //     ease: 'power4.out',
-  //     stagger: {
-  //       amount: 1,
-  //     }
-  //   });
-  // }, []);
-
   const [isSearchOpen, setSearchOpen] = useState(false);
   const [isNavOpen, setNavOpen] = useState(false);
-  const [isProductsOpen, setProductsOpen] = useState(false); // New state for products dropdown
+  const [isProductsOpen, setProductsOpen] = useState(false);
 
   const handleClick = () => {
     window.scrollTo(0, 0);
@@ -60,7 +24,7 @@ const Navbar = () => {
 
   const closeNav = () => {
     setNavOpen(false);
-    setProductsOpen(false); // Close products dropdown when the navigation closes
+    setProductsOpen(false);
   };
 
   const toggleProducts = () => {
@@ -86,8 +50,8 @@ const Navbar = () => {
             link.classList.add('animate');
           }, (index + 1) * 180);
         });
-      }, 350)
-    },0);
+      }, 350);
+    }, 0);
   }, []);
 
   return (
@@ -104,32 +68,28 @@ const Navbar = () => {
           onMouseLeave={toggleProducts}
         >
           <a href="#" className="dropdown-togglele">Products <i className={`uil ${isProductsOpen ? 'uil-angle-up' : 'uil-angle-down'}`}></i></a>
-          {/* Products Dropdown */}
-          {isProductsOpen && (
-            <ul className="dropdown">
-              <li>
-                <Link to="/SE03" smooth={true} duration={500} onClick={() => { closeNav(); handleClick(); }}>
-                  SE03
-                </Link>
-              </li>
-              <li>
-                <Link to="/SE03MAX" smooth={true} duration={500} onClick={() => { closeNav(); handleClick(); }}>
-                  SE03 MAX
-                </Link>
-              </li>
-              <li>
-                <Link to="/SE03LITE" smooth={true} duration={500} onClick={() => { closeNav(); handleClick(); }}>
-                  SE03 LITE
-                </Link>
-              </li>
-            </ul>
-          )}
+          <ul className="dropdown">
+            <li>
+              <Link to="/SE03" onClick={() => { closeNav(); handleClick(); }}>
+                SE03
+              </Link>
+            </li>
+            <li>
+              <Link to="/SE03MAX" onClick={() => { closeNav(); handleClick(); }}>
+                SE03 MAX
+              </Link>
+            </li>
+            <li>
+              <Link to="/SE03LITE" onClick={() => { closeNav(); handleClick(); }}>
+                SE03 LITE
+              </Link>
+            </li>
+          </ul>
         </li>
         <li><Link to="/Compare" onClick={() => { closeNav(); handleClick(); }}>Compare</Link></li>
         <li><Link to='/AboutUs' onClick={() => { closeNav(); handleClick(); }}>About Us</Link></li>
         <li><Link to='/ContactUs' onClick={() => { closeNav(); handleClick(); }}>Contact Us</Link></li>
       </ul>
-
       <button className="search-icon btn btn-nav" id="searchIcon"><a href="https://wa.me/9023987528" target='_blank'> Order Now </a></button>
       <i className="fas fa-bars navOpenBtn" onClick={openNav} id="nav-toggler"></i>
     </nav>
