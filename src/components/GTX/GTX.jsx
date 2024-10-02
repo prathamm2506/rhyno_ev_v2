c1668c7439b7599e9f38a0f459f89889dbe157ab
prathamm2506 <fs20if005@gmail.com>
2024-10-02 19:31:06 +0530

    updates done

--- src/components/GTX/GTX.jsx
+++ src/components/GTX/GTX.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { IoIosArrowDropright } from "react-icons/io";
-import IMG from '../../assets/GTX.png';
-import './GTX.css';
-import Formmain from '../popupform/Formmain';
 import Formmain2 from '../popupform/Formmain2';
+import './GTX.css';
 
 const GTX = () => {
     const handleLearnMoreClick = () => {
@@ -17,28 +15,24 @@ const GTX = () => {
         <div className="gtx-container">
             <div className="gtx-content">
                 <div className="gtx-details">
-                    <h1 className="headerText">The Thrilling Machine</h1>
                     <div className="badge">ALL NEW</div>
                     <h1 className="title">RHYNO GTX Series</h1>
                     <p className="description">
-                        For those who don’t like a boring scooter! Immerse yourself into a never-before-experience. Stand out from the crowd. Now
+                        For those who don’t like a boring scooter! Immerse yourself into a never-before-experience. Stand out from the crowd. Now.
                     </p>
                     <div className="buttons">
-                        {/* <button className="book-now">
-                            <a href="https://wa.me/9023987528" className="book-now-link" target='_blank' rel="noopener noreferrer">
-                                Book Now
-                            </a>
-                            <IoIosArrowDropright size="20px" />
-                        </button> */}
-                        <Formmain2/>
-                        <button className="learn-more" onClick={handleLearnMoreClick}>Learn more</button>
+                        <button className="ordernow">
+                            <Formmain2 />
+                        </button>
+                        <button className="learn-more" onClick={handleLearnMoreClick}>
+                            Learn more
+                        </button>
                     </div>
                 </div>
-                <div className="gtx-image-container">
-                    <img className="gtx-image" src={IMG} alt="GTX Series" />
-                </div>
             </div>
-
+            <div className="gtx-image-container">
+                <img className="gtx-image" src="https://i.ibb.co/g7t4W6f/thrilling-machine-section.png" alt="GTX Series" />
+            </div>
         </div>
     );
 };
