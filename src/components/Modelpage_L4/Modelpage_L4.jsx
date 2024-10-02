b31ae168aaf4bac7e9870fc8a59dc31b05fbfa98
prathamm2506 <fs20if005@gmail.com>
2024-10-02 21:51:19 +0530

    updates done

--- src/components/Modelpage_L4/Modelpage_L4.jsx
+++ src/components/Modelpage_L4/Modelpage_L4.jsx
@@ -1,21 +1,35 @@
 import React from 'react';
 import './Modelpage_L4.css';
-import Img1 from '../../assets/1.png';
-import Img2 from '../../assets/2.png';
 import Img9 from '../../assets/9.png';
 
 const Modelpage_L4 = () => {
     return (
         <div className="custom-bike-layout">
             <div className="custom-top-section">
-                <div className="custom-image-container">
-                    <img src={Img1} alt="Image 1" className="custom-top-image" />
+                <div className="custom-video-container">
+                    <iframe 
+                        width="100%" 
+                        height="450"  
+                        src="https://www.youtube.com/embed/dQw4w9WgXcQ" 
+                        title="YouTube video 1" 
+                        frameBorder="0" 
+                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+                        allowFullScreen
+                    ></iframe>
                     <p className="custom-description-text">
                         LOREM IPSUM IS SIMPLY DUMMY TEXT OF THE PRINTING AND TYPESETTING INDUSTRY.
                     </p>
                 </div>
-                <div className="custom-image-container">
-                    <img src={Img2} alt="Image 2" className="custom-top-image" />
+                <div className="custom-video-container">
+                    <iframe 
+                        width="100%" 
+                        height="450"  
+                        src="https://www.youtube.com/embed/tgbNymZ7vqY" 
+                        title="YouTube video 2" 
+                        frameBorder="0" 
+                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+                        allowFullScreen
+                    ></iframe>
                     <p className="custom-description-text">
                         LOREM IPSUM IS SIMPLY DUMMY TEXT OF THE PRINTING AND TYPESETTING INDUSTRY.
                     </p>
